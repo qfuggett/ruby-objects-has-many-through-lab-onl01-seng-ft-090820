6db33f189347a67fcbd0204696d1d459a5cf27d6
QueenTesa Fuggett <qfuggett@gmail.com>
2020-09-26 14:21:11 +0000

    Automatically backed up by Learn

---
+++ lib/genre.rb
@@ -0,0 +1,27 @@
+class Genre
+  attr_accessor :name, :song, :artist
+  
+  @@all = [ ]
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def songs
+    Song.all.select do |song|
+      song.genre == self
+    end
+  end
+    
+    def artists
+      Song.select do |artist|
+        song.artist
+      end
+    end
+  
+end
