228cb3566416de58d69687ad598ef81cd1cd64f9
QueenTesa Fuggett <qfuggett@gmail.com>
2020-09-26 15:50:43 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -9,6 +9,27 @@ class Patient
   end
   
   
+  def self.all
+    @@all
+  end
   
+  
+  def new_appointment(date, Doctor)
+    Appointment.new(name)
+  end
+  
+  
+  def appointments
+    Appointment.all.select do |element|
+      element.patient == self
+    end
+  end
+  
+  
+  def doctors
+    appointments.collect do |element|
+      element.doctor
+    end
+  end
   
 end
