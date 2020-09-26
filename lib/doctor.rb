3b912b6367751aaab47de4b6cc0e81a7cb76fb41
QueenTesa Fuggett <qfuggett@gmail.com>
2020-09-26 15:43:36 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,10 +1,21 @@
 class Doctor
-  attr_accessor :name
+  attr_accessor :name, :patient
+  
+  @@all = [ ]
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
   
   
+  def new_appointment(date, patient)
+    Appointment.new()
+  end
   
-  
-  
+  def appointments
+    Appointment.all.select do |element|
+      element
   
   
 end
